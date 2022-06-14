9484e80c8e71e7d79cc9717b182b22769a3adbeb
nahushz99 <nahushzilpilwar@gmail.com>
2022-06-14 11:53:26 +0530

    Project Commite

---
+++ GoogalCaptureAuth-1/src/main/java/com/app/GoogalCaptureAuth1Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogalCaptureAuth1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogalCaptureAuth1Application.class, args);
+	}
+
+}
